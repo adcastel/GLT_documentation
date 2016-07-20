c00da22ad05b8209696203d4b41aa79ed57af701
Adrian Castello <adcastel@uji.es>
2016-07-20 08:31:14 +0000

    script para autogenerar doc

---
+++ doc_broadcast.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+doxygen glt_doc.h
+
+cd latex
+
+make clean && make
+
+cp refman.pdf ../
+
+cd ..
+
+scp -r html/* adcastel@lynx.uji.es:www/GLT_doc/
+
+
+cp refman.pdf $(HOME)/GLT/doc/
